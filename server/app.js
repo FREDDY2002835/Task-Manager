3446b3c5ed177051435bc338bb0df9af42621303
Frederick Muhimuzi <muhimuzifreddy059@gmail.com>
2026-07-22 14:15:39 +0300

    front

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+
+const cors = require("cors");
+
+
+const app = express();
+
+
+app.use(cors());
+
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("Task Manager API");
+});
+
+
+module.exports = app;
