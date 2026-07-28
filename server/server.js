a9b388a869ce28f34d2965418f19e703225970a8
Frederick Muhimuzi <muhimuzifreddy059@gmail.com>
2026-07-28 13:22:52 +0300

    backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,20 @@
-const app = require("./app");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("TaskFlow API is running...");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
